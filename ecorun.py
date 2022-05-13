e539e84dc160950866c40e22e29e650afc57fdc0
Alexey Kozlov <alexey.kozlov@h-its.org>
2022-05-13 18:09:36 +0200

    ecorun: print results to a CSV file

--- ecorun.py
+++ ecorun.py
@@ -34,6 +34,8 @@ if __name__ == '__main__':
 
 #  print(sys.argv)
   
+  outfile = None
+  runname = "noname"
   cmdline_start = 1
   if len(sys.argv) > 3 and sys.argv[1] == "-p":
     gov = sys.argv[2]
@@ -45,6 +47,14 @@ if __name__ == '__main__':
     cmdline_start += 2
   else:
     old_gov = None
+
+  if len(sys.argv) > 5 and sys.argv[3] == "-o":
+    outfile = sys.argv[4]
+    cmdline_start += 2
+
+  if len(sys.argv) > 7 and sys.argv[5] == "-n":
+    runname = sys.argv[6]
+    cmdline_start += 2
  
   cmdline = sys.argv[cmdline_start:]
   
@@ -75,5 +85,17 @@ if __name__ == '__main__':
   print("energy_j:  ", round(diff_joules, 3))
   print("energy_kwh:", round(diff_kwh, 3))
   print("co2_g:     ", round(diff_co2, 3))
+  
+  if outfile:
+    if not os.path.exists(outfile):
+      with open(outfile, "w") as f:
+        headers = ["name", "policy", "time_s", "pwr_avg_w", "energy_j", "energy_kwh", "co2_g"]
+        f.write(",".join(headers) + "\n");
+      
+    with open(outfile, "a") as f:
+      vals = [runname, gov]
+      res = [diff_time, avg_pwr, diff_joules, diff_kwh, diff_co2]
+      vals += [str(round(x, 3)) for x in res]
+      f.write(",".join(vals) + "\n")
 
 
